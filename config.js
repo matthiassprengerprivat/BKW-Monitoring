d2a34f05861f8690e643c78f9b6db030149f3e04
MatthiasSprenger <matthias@hyrox.com>
2026-08-18 11:59:49 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// Euer persönlicher Link zur veröffentlichten Google-Tabelle (Log-Tab, als CSV).
+// Diese Datei bitte NIE komplett ersetzen, wenn ihr eine neue index.html bekommt -
+// nur den Link unten bei Bedarf aktualisieren (z. B. wenn ihr neu "Im Web veröffentlichen" macht).
+//
+// WICHTIG: Der Link unten ist der letzte Stand, den wir gemeinsam eingerichtet haben. Bitte einmal
+// prüfen, dass es wirklich der Link zu eurem "Log"-Tab ist (nicht "Ganzes Dokument"), bevor ihr
+// diese Datei ins Repo hochladet - ihr wisst selbst am besten, welcher Link bei euch gerade aktiv ist.
+var CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT5Bdep76NZKMNqet9gaRuY_Hv2f0Mc8vt1jUDOYm-53_6asyMWyt4Fi6JhqrGWE4-YXYTjvm8c1pi8/pub?gid=1528432315&single=true&output=csv";
+ 
