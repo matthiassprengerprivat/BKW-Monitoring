2a6673427ed45eb6016f96319d797a53d35cc7c0
matthiassprengerprivat <matthias.sprenger@yahoo.de>
2026-08-20 08:24:52 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 // WICHTIG: Der Link unten ist der letzte Stand, den wir gemeinsam eingerichtet haben. Bitte einmal
 // prüfen, dass es wirklich der Link zu eurem "Log"-Tab ist (nicht "Ganzes Dokument"), bevor ihr
 // diese Datei ins Repo hochladet - ihr wisst selbst am besten, welcher Link bei euch gerade aktiv ist.
-var CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT5Bdep76NZKMNqet9gaRuY_Hv2f0Mc8vt1jUDOYm-53_6asyMWyt4Fi6JhqrGWE4-YXYTjvm8c1pi8/pub?gid=1528432315&single=true&output=csv";
+var CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMWqWqdLYCETDjgfgTuvcgwBu4cR0F_NDw45jHOiYlwhHjYNnGDS9vku6sjhlB27mdfmBRWCNSlMN8/pub?gid=1978022599&single=true&output=csv";
  
